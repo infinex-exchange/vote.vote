f789dfc24c0716d1f85d387d167f5ed2dadb859e
infinex-exchange <admin@infinex.cc>
2023-10-23 17:42:20 +0200

    Maybe create vote

--- src/Votings.php
+++ src/Votings.php
@@ -31,7 +31,7 @@ class Votings {
                 $th -> maybeCreateVoting();
             }
         );
-        maybeCreateVoting();
+        $this -> maybeCreateVoting();
         
         $promises = [];
         
